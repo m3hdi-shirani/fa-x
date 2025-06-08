2e77e253f55ab502533f107fd6d29ea54ec14919
Mehdi Shirani <m3hdi.trader@gmail.com>
2025-06-08 10:13:46 +0330

    Build custom breakpoint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,14 @@ export default function RootLayout({
   return (
     <html dir="rtl" lang="fa">
       <body>
-        <div className="flex justify-between">
-          <div>
+        <div className="flex justify-between mx-auto max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl">
+          <div className="px-2 sm:px-4 xxl:px-8  h-screen">
             <RightBar />
           </div>
-          <div>{children}</div>
-          <div>
+          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-gray-100 h-screen">
+            {children}
+          </div>
+          <div className="hidden lg:flex mr-4 xl:mr-8  h-screen">
             <LeftBar />
           </div>
         </div>
