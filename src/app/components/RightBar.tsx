e74d01b53580696d88662bd2c14426af2d943e24
Mehdi Shirani <m3hdi.trader@gmail.com>
2025-06-08 11:17:52 +0330

    Responsive layout and building the right side of the site

--- src/app/components/RightBar.tsx
+++ src/app/components/RightBar.tsx
@@ -1,7 +1,120 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
+const menuList = [
+  {
+    id: 1,
+    name: "خانه",
+    link: "/",
+    icon: "home.svg",
+  },
+  {
+    id: 2,
+    name: "کاوش",
+    link: "/",
+    icon: "explore.svg",
+  },
+  {
+    id: 3,
+    name: "آگاه‌سازی‌ها",
+    link: "/",
+    icon: "notification.svg",
+  },
+  {
+    id: 4,
+    name: "پیام‌ها",
+    link: "/",
+    icon: "message.svg",
+  },
+  {
+    id: 5,
+    name: "نشانک‌ها",
+    link: "/",
+    icon: "bookmark.svg",
+  },
+  {
+    id: 6,
+    name: "شغل‌ها",
+    link: "/",
+    icon: "job.svg",
+  },
+  {
+    id: 7,
+    name: "انجمن‌ها",
+    link: "/",
+    icon: "community.svg",
+  },
+  {
+    id: 8,
+    name: "نمایه",
+    link: "/",
+    icon: "profile.svg",
+  },
+  {
+    id: 9,
+    name: "بیشتر",
+    link: "/",
+    icon: "more.svg",
+  },
+];
+
 const RightBar = () => {
-  return <div>RightBar</div>;
+  return (
+    <div className="h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8">
+      {/* Logo|Menu|Btn */}
+      <div className="flex flex-col gap-4 text-lg items-center xxl:items-start">
+        {/* Logo */}
+        <Link href="/" className="p-2">
+          <Image src={"icons/logo.svg"} alt="لوگوی X" height={24} width={24} />
+        </Link>
+        {/* List Menu */}
+        <div className="flex flex-col gap-4">
+          {menuList.map((item) => (
+            <Link
+              className="flex items-center gap-4 p-2 rounded-full hover:bg-[#141414]"
+              href={item.link}
+              key={item.id}
+            >
+              <Image
+                src={`icons/${item.icon}`}
+                alt={item.name}
+                width={24}
+                height={24}
+              />
+              <span className="hidden xxl:inline">{item.name}</span>
+            </Link>
+          ))}
+          {/* Btn */}
+          <Link
+            href="/"
+            className="xxl:hidden bg-white text-black rounded-full w-12 h-12 flex item-center justify-center"
+          >
+            <Image src="icons/post.svg" alt="پست جدید" width={24} height={24} />
+          </Link>
+          <Link
+            href="/"
+            className="hidden xxl:block bg-white rounded-full text-black  py-2 px-20 font-bold"
+          >
+            پست کردن
+          </Link>
+        </div>
+      </div>
+      {/* Information account */}
+      <div className="flex items-center justify-center">
+        <div className="flex items-center gap-2">
+          <div className="relative h-10 w-10 rounded-full overflow-hidden">
+            <Image src="/general/avatar.png" alt="mehdi shirani" fill />
+          </div>
+          <div className="hidden xxl:flex flex-col">
+            <span className="font-bold">Mehdi Shirani</span>
+            <span className=" text-textGray">m3hdi.shirani@gmail.com</span>
+          </div>
+        </div>
+        <div className="hidden xxl:block cursor-pointer font-bold">...</div>
+      </div>
+    </div>
+  );
 };
 
 export default RightBar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,13 +11,13 @@ export default function RootLayout({
     <html dir="rtl" lang="fa">
       <body>
         <div className="flex justify-between mx-auto max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl">
-          <div className="px-2 sm:px-4 xxl:px-8  h-screen">
+          <div className="px-2 sm:px-4 xxl:px-8">
             <RightBar />
           </div>
-          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-gray-100 h-screen">
+          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-gray-100">
             {children}
           </div>
-          <div className="hidden lg:flex flex-1 mr-4 xl:mr-8  h-screen">
+          <div className="hidden lg:flex flex-1 mr-4 xl:mr-8">
             <LeftBar />
           </div>
         </div>
