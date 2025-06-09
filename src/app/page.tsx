5bf7a2e87ac03d20a5be24f0d0f34fcdf0597174
Mehdi Shirani <m3hdi.trader@gmail.com>
2025-06-09 10:07:53 +0330

    Complete homepage design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
+import Link from "next/link";
+import Share from "./components/Share";
+import Feed from "./components/Feed";
+
 const Homepage = () => {
-  return <span>صفحه اصلی</span>;
+  return (
+    <div>
+      <div className="px-4 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-textGray">
+        <Link
+          href=""
+          className="pb-3 flex items-center border-b-4 border-iconBlue"
+        >
+          link
+        </Link>
+        <Link href="" className=" hidden md:flex pb-3  items-center">
+          دنبال شده
+        </Link>
+        <Link href="" className=" hidden md:flex pb-3  items-center">
+          React
+        </Link>
+        <Link href="" className=" hidden md:flex pb-3  items-center">
+          Next
+        </Link>
+      </div>
+      <Share />
+      <Feed />
+    </div>
+  );
 };
 
 export default Homepage;
