f670e63ead5ec76f6b7658566c3aaa94196ef572
Mehdi Shirani <m3hdi.trader@gmail.com>
2025-06-09 10:07:00 +0330

    Create Post component

---
+++ src/app/components/PostInfo.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Image from "./Image";
+
+const PostInfo = () => {
+  return (
+    <div className="cursor-pointer w-4 h-4 relative">
+      <Image src="icons/infoMore.svg" alt="" w={16} h={16} />
+    </div>
+  );
+};
+
+export default PostInfo;
